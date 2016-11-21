da73f522a957263b59131fe45c1ffb6e3734515c
Jose Luis Poveda <jlpoveda@gmail.com>
2016-11-21 19:14:02 +0100

    Testing remote script

---
+++ install/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
+
+#  git config --global user.email "you@example.com"
+#  git config --global user.name "Your Name"
+
